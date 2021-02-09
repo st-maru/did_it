5d7876edc23a8c9ef0e7cf9807a3baf244b39d88
st-maru <soma.tamura@gmail.com>
2021-02-09 15:09:40 +0900

    タスク編集機能の実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -17,6 +17,19 @@ class TasksController < ApplicationController
     end
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      redirect_to task_completions_path(params[:id])
+    else
+      render :edit
+    end
+  end
+
   private
   def task_params
     params.require(:task).permit(:name, :goal).merge(user_id: current_user.id)
