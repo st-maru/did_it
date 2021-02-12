e4cf48cec8c12d324ed3d34d0f33b3897d9003e7
st-maru <soma.tamura@gmail.com>
2021-02-12 11:54:18 +0900

    chartkickの導入

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,6 +11,8 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("../working_time_count")
+require("chartkick")
+require("chart.js")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
