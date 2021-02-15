18906c02d6dfa942bfdd320ff052490aa7ac0756
st-maru <soma.tamura@gmail.com>
2021-02-16 00:32:12 +0900

    一覧表示機能の実装

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,5 +1,5 @@
 class ListsController < ApplicationController
   def index
-    @task = Task.order(id: "DESC").page(params[:page]).per(10)
+    @task = Task.includes(:user).order(id: "DESC").page(params[:page]).per(10)
   end
 end
