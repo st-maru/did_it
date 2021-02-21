a3d0ce9eb02e397060e4bed9e64e5f85f580b58e
st-maru <soma.tamura@gmail.com>
2021-02-22 03:07:42 +0900

    タブ機能の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
+    @user_tasks = @user.tasks.page(params[:page]).per(5)
     @favorite_tasks = @user.favorite_tasks.page(params[:page]).per(5)
   end
 end
