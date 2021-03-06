91b176bbbc2f24b360aac2aef728e92e6f2a37ee
st-maru <soma.tamura@gmail.com>
2021-03-06 20:05:53 +0900

    リファクタリング

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,18 +1,8 @@
 class ListsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_q
 
   def index
     @task = Task.includes(:user).order(id: 'DESC').page(params[:page]).per(10)
   end
 
-  def search
-    @results = @q.result.page(params[:page]).per(10)
-  end
-
-  private
-
-  def set_q
-    @q = Task.ransack(params[:q])
-  end
 end
