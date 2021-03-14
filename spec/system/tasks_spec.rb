447bbff718dd466130c9fe4c7fe876617263f543
st-maru <soma.tamura@gmail.com>
2021-03-14 19:18:39 +0900

    タスク削除の結合テストコード

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -107,3 +107,56 @@ RSpec.describe 'タスク編集', type: :system do
   end
 end
 
+RSpec.describe 'タスク削除', type: :system do
+  before do
+    @task1 = FactoryBot.create(:task)
+    @task2 = FactoryBot.create(:task)
+  end
+  context 'タスク削除ができるとき' do
+    it 'ログインしたユーザーは自らが作成したタスクの削除ができる' do
+      # ツイート1を投稿したユーザーでログインする
+      visit new_user_session_path
+      fill_in 'メールアドレス', with: @task1.user.email
+      fill_in 'パスワード', with: @task1.user.password
+      find('input[name="commit"]').click
+      #マイページに移動する
+      visit tasks_path
+      # タスク1ページに移動する
+      click_on @task1.name
+      # ツイート1に「削除」ボタンがあることを確認する
+      expect(page).to have_link href: task_path(@task1)
+      # 投稿を削除するとレコードの数が1減ることを確認する
+      expect{
+        find_link(href: task_path(@task1)).click
+      }.to change { Task.count }.by(-1)
+      # マイページに遷移する
+      visit tasks_path
+      # マイページにはタスク1の内容が存在しないことを確認する
+      expect(page).to have_no_content("#{@task1.name}")
+      expect(page).to have_no_content("#{@task1.goal}")
+    end
+  end
+  context 'タスク削除ができないとき' do
+    it 'ログインしたユーザーは自分以外が作成したタスクの削除ができない' do
+      # タスク1を投稿したユーザーでログインする
+      visit new_user_session_path
+      fill_in 'メールアドレス', with: @task1.user.email
+      fill_in 'パスワード', with: @task1.user.password
+      find('input[name="commit"]').click
+      #タスク一覧ページに移動する
+      visit lists_path
+      # タスク2ページに移動する
+      click_on @task2.name
+      # タスク2に「削除」ボタンが無いことを確認する
+      expect(page).to have_no_link href: edit_task_path(@task2)
+    end
+    it 'ログインしていないとタスクの削除ができない' do
+      # トップページにいる
+      visit root_path
+      # マイページへ移動する
+      visit tasks_path
+      # ログイン画面へ移動させられる
+      expect(current_path).to eq(new_user_session_path)
+    end
+  end
+end
