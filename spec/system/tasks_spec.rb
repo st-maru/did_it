add81308f16b848ab2e753496f2dc9a9a58678d4
st-maru <soma.tamura@gmail.com>
2021-03-14 19:17:28 +0900

    タスク編集の結合テストコード

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -41,3 +41,69 @@ RSpec.describe 'タスク投稿', type: :system do
   end
 end
 
+RSpec.describe 'タスク編集', type: :system do
+  before do
+    @task1 = FactoryBot.create(:task)
+    @task2 = FactoryBot.create(:task)
+  end
+  context 'タスク編集ができるとき' do
+    it 'ログインしたユーザーは自分が作成したタスクの編集ができる' do
+      # ツイート1を投稿したユーザーでログインする
+      visit new_user_session_path
+      fill_in 'メールアドレス', with: @task1.user.email
+      fill_in 'パスワード', with: @task1.user.password
+      find('input[name="commit"]').click
+      # タスクページに移動する
+      click_on @task1.name
+      # タスク1に「編集」ボタンがあることを確認する
+      expect(page).to have_link href: edit_task_path(@task1)
+      # 編集ページへ遷移する
+      visit edit_task_path(@task1)
+      # すでに投稿済みの内容がフォームに入っていることを確認する
+      expect(
+        find('#task_name').value
+      ).to eq(@task1.name)
+      expect(
+        find('#task_goal').value
+      ).to eq(@task1.goal)
+      # 投稿内容を編集する
+      fill_in 'タスク名', with: "#{@task1.name}+編集したタスク名"
+      fill_in '目標', with: "#{@task1.goal}+編集した目標"
+      # 編集してもTaskモデルのカウントは変わらないことを確認する
+      expect{
+        find('input[value="変更する"]').click
+      }.to change { Task.count }.by(0)
+      # タスクページに遷移したことを確認する
+      expect(current_path).to eq(task_completions_path(@task1))
+      # マイページに遷移する
+      visit tasks_path
+      # マイページには先ほど変更した内容のタスクが存在することを確認する
+      expect(page).to have_content("#{@task1.name}+編集したタスク名")
+      expect(page).to have_content("#{@task1.goal}+編集した目標")
+    end
+  end
+  context 'タスク編集ができないとき' do
+    it 'ログインしたユーザーは自分以外が投稿したタスクの編集画面には遷移できない' do
+      # タスク1を投稿したユーザーでログインする
+      visit new_user_session_path
+      fill_in 'メールアドレス', with: @task1.user.email
+      fill_in 'パスワード', with: @task1.user.password
+      find('input[name="commit"]').click
+      #タスク一覧ページに移動する
+      visit lists_path
+      # タスク2ページに移動する
+      click_on @task2.name
+      # タスク2に「編集」ボタンがないことを確認する
+      expect(page).to have_no_link href: edit_task_path(@task2)
+    end
+    it 'ログインしていないとタスクの編集画面には遷移できない' do
+      # トップページにいる
+      visit root_path
+      # マイページへ移動する
+      visit tasks_path
+      # ログイン画面へ移動させられる
+      expect(current_path).to eq(new_user_session_path)
+    end
+  end
+end
+
